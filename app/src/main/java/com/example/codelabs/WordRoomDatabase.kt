c44eaf5880f5e6b0bc1f5fa085fd6af2cc1f2e8c
Aldiriansyah Dwi <kurosenpai12@gmail.com>
2022-04-05 23:51:25 +0700

    add the activity for input new word

--- app/src/main/java/com/example/codelabs/WordRoomDatabase.kt
+++ app/src/main/java/com/example/codelabs/WordRoomDatabase.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 // exportschema diberikan false supaya menghindari peringatan build
 @Database (entities = [Word::class], version = 1, exportSchema = false)
@@ -19,16 +22,42 @@ abstract class WordRoomDatabase : RoomDatabase() {
 
         // disini akan membuat singleton, dimana akan membuat database saat
         // pertama kali dkases
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             return INSTANCE ?: synchronized(this){
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "word_database"
-                ).build()
+                )
+                    .addCallback(WordDatabaseCallback(scope))
+                    .build()
                 INSTANCE = instance
                 instance
             }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ): RoomDatabase.Callback(){
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let{database->
+                scope.launch{
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao){
+            // menghapus seluruh konten
+            wordDao.deleteAll()
+
+            // menambahkan sampel
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
         }
     }
 
